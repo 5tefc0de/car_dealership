89590609828e30a2f93a9c05683d1741dca0810f
5tefc0de <st.muhle@gmail.com>
2019-12-30 18:21:17 +0100

    Add some code

--- car_dealership/src/car_dealership/Dealership.java
+++ car_dealership/src/car_dealership/Dealership.java
@@ -1,5 +1,20 @@
 package car_dealership;
 
 public class Dealership {
+	
+	public static void main(String[] args) {
+		
+		Customer cust1 = new Customer("Tom", "123 Anything St. ", 200);
+		
+		Vehicle vehicle = new Vehicle("Honda", "Accord", 2000);
+		Vehicle car = new Vehicle("BMW", "M3", 20000);
+		
+		Employee emp = new Employee();
+		
+		cust1.purchaseCar(vehicle, emp, true);
+		
+		boolean value = car.equals(vehicle);
+		System.out.println(value);
+	}
 
 }
